720443e0683792b8efc893ef4edc032642d0a122
Charles <hallard04@free.fr>
2018-03-12 17:41:29 +0100

    Added RN2483 Firmware 1.0.4

--- HexFileImage.h
+++ HexFileImage.h
@@ -8,6 +8,7 @@
 // Uncomment one of the following, and one only, not more, not less.
 //#define HEXFILE_RN2483_101
 //#define HEXFILE_RN2483_103
+#define HEXFILE_RN2483_104
 //#define HEXFILE_RN2903AU_097rc7
 //#define HEXFILE_RN2903_098
 //#define HEXFILE_RN2903_103
@@ -16,6 +17,8 @@
 #include "HexFileImage2483_101.h"
 #elif defined(HEXFILE_RN2483_103)
 #include "HexFileImage2483_103.h"
+#elif defined(HEXFILE_RN2483_104)
+#include "HexFileImage2483_104.h"
 #elif defined(HEXFILE_RN2903AU_097rc7)
 #include "HexFileImage2903AU_097rc7.h"
 #elif defined(HEXFILE_RN2903_098)
@@ -23,7 +26,7 @@
 #elif defined(HEXFILE_RN2903_103)
 #include "HexFileImage2903_103.h"
 #else
-#error "Please define one the the following: HEXFILE_RN2483_101, HEXFILE_RN2483_103, HEXFILE_RN2903AU_097rc7, HEXFILE_RN2903_098, , HEXFILE_RN2903_103"
+#error "Please define one the the following: HEXFILE_RN2483_101, HEXFILE_RN2483_103, HEXFILE_RN2483_104, HEXFILE_RN2903AU_097rc7, HEXFILE_RN2903_098, , HEXFILE_RN2903_103"
 #endif
 
 
